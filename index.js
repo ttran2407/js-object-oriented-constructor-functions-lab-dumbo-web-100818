dc69298a84fa81af13315cc776f2be6e0cc08de1
Tien Tran <tientran2407.vn@gmail.com>
2019-04-12 20:46:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-Scooter = () => {
-  
+Scooter = (year, color, model) => {
+  this.year = year;
+  this.color = color;
+  this.model = model
+}
+
+Driver = (name, age, experience) => {
+  this.name = name;
+  this.age = age;
+  this.experience = experience;
+}
+
+PickupLocation = (address, city) => {
+  this.address = address;
+  this.city = city;
 }
